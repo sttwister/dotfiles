3f81da4a1e84fdeba48eefa69323c347bfc68271
sttwister <felix@rebs-group.com>
2022-04-05 19:16:47 +0300

    Fixed oh-my-zsh dir and install command-not-found

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -2,11 +2,12 @@
 
 # Install packages
 sudo apt -y install \
+	command-not-found \
 	git \
 	vim \
 	zsh
 
 # Install oh-my-zsh
-KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+CHSH=yes KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
