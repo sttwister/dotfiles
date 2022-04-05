044cc3e578931ae194813860e052e7441a52fecb
sttwister <felix@rebs-group.com>
2022-04-05 19:05:20 +0300

    Added --noinput to apt install

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install packages
-sudo apt install git zsh
+sudo apt install --noinput git zsh
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
