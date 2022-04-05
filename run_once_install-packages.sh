eaeb3ae21ac85841d8bc21ea9df99a72b319a54c
sttwister <felix@rebs-group.com>
2022-04-05 19:10:15 +0300

    Don't replace .zshrc after installing oh-my-zsh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -7,6 +7,6 @@ sudo apt -y install \
 	zsh
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
