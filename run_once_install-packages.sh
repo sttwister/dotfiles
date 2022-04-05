32192333e35247d29fef6c32f92cdbb906b8a7a6
sttwister <felix@rebs-group.com>
2022-04-05 19:09:52 +0300

    Added vim. More readable apt install

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Install packages
-sudo apt -y install git zsh
+sudo apt -y install \
+	git \
+	vim \
+	zsh
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
