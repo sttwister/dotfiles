e78af79dbe79bf0d856771a148c29beb8fd601b6
sttwister <felix@rebs-group.com>
2022-04-05 19:04:29 +0300

    Added git

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install packages
-sudo apt install zsh
+sudo apt install git zsh
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
